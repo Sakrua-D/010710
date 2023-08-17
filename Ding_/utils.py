cae8ded1d48a44abbce60f32816e9098a0447e99
DingZewei <2529707396@qq.com>
2023-08-17 17:04:09 +0800

    utils

--- Ding_/utils.py
+++ Ding_/utils.py
@@ -14,7 +14,7 @@ def daoguanwang():
             for i in range(10000):
                 print('变态王大笑一声并再次向你发起了视频邀请：')
                 a = input('请输入1接受邀请，输入2拒绝邀请：')
-                if a == 1 and i % 1000 == 0:
+                if a == '1' and i % 1000 == 0:
                     print(f'变态王开始导管了！他导了{i + 1}下！！！')
                 else:
                     print('请正确输入！')
